8ab36916b4452e3b4d134cc738cffb2d3eebd179
shing100 <shing100@naver.com>
2024-08-05 08:30:34 +0900

    setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="An OpenAI API compatible server for locally running transformers models",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/transformers-openai-api",
+    url="https://github.com/shing100/transformers-openai-api",
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
@@ -23,6 +23,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     python_requires=">=3.7",
     install_requires=[
